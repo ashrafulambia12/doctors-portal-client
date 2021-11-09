7c1f5513dbb016e33b15d854026cc0f4d90225a4
adil <ashrafulambiaadil11@gmail.com>
2021-11-09 23:12:43 +0600

    first commi tt

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -13,7 +13,7 @@ const Appointments = ({ date }) => {
     const [appointments, setAppointments] = useState([]);
 
     useEffect(() => {
-        const url = `https://warm-sands-63288.herokuapp.com/appointments?email=${user.email}&date=${date}`
+        const url = `https://warm-sands-63288.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`
         fetch(url, {
             headers: {
                 'authorization': `Bearer ${token}`
